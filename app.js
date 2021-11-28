fbe6b3ac19eaa4e8f0f7c1a1a9197c74308adfda
igrimzyi <1isaiahofficial1@gmail.com>
2021-11-28 15:05:56 -0800

    Added Mate-ejs
    Mate-ejs for layouts of all my view pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate= require('ejs-mate'); 
 const methodOverride = require('method-override');
 const coffeeShop = require('./models/coffee');
 
@@ -16,6 +17,7 @@ db.once('open', () =>{
 })
 const app = express(); 
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views' , path.join(__dirname, 'views'))
 

30cd593e0b4267e44260926ad9fe2e5639d2a52a
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-07 14:05:16 -0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req,res)=>{
     res.render('users/register');
 })
-router.post('/register', async(req,res)=>{
-    res.send(req.body)
-})
+router.post('/register', catchAsync(async(req,res)=>{
+    try{
+    const {email, username, password} = req.body;
+    const user = new User({email,username});
+    const registeredUser =await User.register(user, password);
+    req.flash('success' ,'Welcome to Brewlicious!')
+    req.redirect('/coffeeShops');
+    } catch(e){
+        req.flash('error', e.message)
+        res.redirect('register');
+    }
+}))
 
 module.exports = router;

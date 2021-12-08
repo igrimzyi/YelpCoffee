43c535c4da0b019645bfeeda15391832e0616a8c
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-08 15:30:58 -0800

    User Routes Controllers

---
+++ controllers/users.js
@@ -0,0 +1,39 @@
+const User = require('../models/user')
+
+module.exports.renderRegister = (req,res)=>{
+    res.render('users/register');
+};
+
+module.exports.register = async(req,res, next)=>{
+    try{
+    const {email, username, password} = req.body;
+    const user = new User({email,username});
+    const registeredUser =await User.register(user, password);
+    req.login(registeredUser, err =>{
+        if(err) return next(err);
+        req.flash('success', 'Successfully Registered and Logged in')
+        res.redirect('/coffeeShops')
+    })
+    req.flash('success' ,'Welcome to Brewlicious!')
+    req.redirect('/coffeeShops');
+    } catch(e){
+        req.flash('error', e.message)
+        res.redirect('register');
+    }
+}
+
+module.exports.renderLogin = (req,res)=>{
+    res.render('users/login');
+}
+module.exports.login = (req,res) =>{
+    req.flash('success', 'Welcome Back!');
+    const redirectUrl = req.session.returnTo || '/coffeeShops'
+    delete req.session.returnTo;
+    res.redirect(redirectUrl)
+};
+module.exports.logout = (req,res)=>{
+    req.logout();
+    req.flash('success', 'Goodbye!');
+    res.redirect('/coffeeShops')
+
+};
--- routes/users.js
+++ routes/users.js
@@ -3,43 +3,15 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const passport = require('passport');
+const users = require('../controllers/users')
 
-router.get('/register', (req,res)=>{
-    res.render('users/register');
-})
-router.post('/register', catchAsync(async(req,res, next)=>{
-    try{
-    const {email, username, password} = req.body;
-    const user = new User({email,username});
-    const registeredUser =await User.register(user, password);
-    req.login(registeredUser, err =>{
-        if(err) return next(err);
-        req.flash('success', 'Successfully Registered and Logged in')
-        res.redirect('/coffeeShops')
-    })
-    req.flash('success' ,'Welcome to Brewlicious!')
-    req.redirect('/coffeeShops');
-    } catch(e){
-        req.flash('error', e.message)
-        res.redirect('register');
-    }
+router.get('/register', users.renderRegister);
+router.post('/register', catchAsync(users.register))
 
-router.get('/login' , (req,res)=>{
-    res.render('users/login');
-})
+router.get('/login' , users.renderLogin)
 
-router.post('/login' , passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}) ,(req,res) =>{
-    req.flash('success', 'Welcome Back!');
-    const redirectUrl = req.session.returnTo || '/coffeeShops'
-    delete req.session.returnTo;
-    res.redirect(redirectUrl)
-})
+router.post('/login' , passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}) , users.login )
 
-router.get('/logout', (req,res)=>{
-    req.logout();
-    req.flash('success', 'Goodbye!');
-    res.redirect('/coffeeShops')
-
-})
+router.get('/logout', users.logout)
 
 module.exports = router;

64e8d9c95403af07fee3ab3085bdec88df82c61c
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-10 17:03:00 -0800

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -3,11 +3,12 @@ module.exports.coffeeShopSchema= Joi.object({
     coffeeShop: Joi.object({
         title: Joi.string().required(), 
         price: Joi.number().required().min(0),
-        image: Joi.string().required(),
+        //image: Joi.string().required(),
         location: Joi.string().required(), 
         description: Joi.string().required()
 
-    }).required() 
+    }).required() ,
+    deleteImages: Joi.array()
 });
 
 module.exports.reviewSchema = Joi.object({

73c3002befeec125eadb683751d25629da8a04a1
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-10 17:03:10 -0800

    Update coffeeShops.js

--- routes/coffeeShops.js
+++ routes/coffeeShops.js
@@ -4,23 +4,21 @@ const coffeeshops = require('../controllers/coffeeshops')
 const catchAsync = require('../utils/catchAsync');
 const coffeeShop = require('../models/coffee');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'})
+const {storage} = require('../cloudinary');
+const upload = multer({storage})
 
 const {isLoggedIn, isAuthor, validateCoffeeShop} = require('../middleware')
 
 router.route('/')
     .get(catchAsync(coffeeshops.index))
-    // .post(isLoggedIn, validateCoffeeShop, catchAsync(coffeeshops.createShop))
-    .post(upload.array('image'), (req,res)=>{
-        res.send(req.body)
-    })
-
+    .post(isLoggedIn,upload.array('image'),validateCoffeeShop, catchAsync(coffeeshops.createShop))
+    
 
 router.get('/new', isLoggedIn, coffeeshops.renderNewForm)
 
 router.route('/:id')
     .get( catchAsync(coffeeshops.showShop))
-    .put(isLoggedIn, isAuthor, validateCoffeeShop, catchAsync(coffeeshops.updateShop))
+    .put(isLoggedIn, isAuthor,upload.array('image'), validateCoffeeShop, catchAsync(coffeeshops.updateShop))
     .delete(isLoggedIn, isAuthor, catchAsync(coffeeshops.deleteShop));
 
 router.get('/:id/edit', isLoggedIn,isAuthor, catchAsync(coffeeshops.renderEditForm));

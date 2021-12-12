a587118cfc72d6018d9b807afc01fda46836bcd0
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-11 20:16:06 -0800

    node modules

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -4,12 +4,14 @@
 	// https://account.mapbox.com
 	mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-container: 'map',
+container: 'cluster-map',
 style: 'mapbox://styles/mapbox/dark-v10',
 center: [-103.5917, 40.6699],
 zoom: 3
 });
  
+
+map.addControl(new mapboxgl.NavigationControl())
 map.on('load', () => {
 // Add a new source from our GeoJSON data and
 // set the 'cluster' option to true. GL-JS will

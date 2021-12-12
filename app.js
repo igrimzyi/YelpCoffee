cd7103804f429073305c3e002b34d25895049d9a
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-11 22:08:16 -0800

    script

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error' , {err});
 })
 
-
-app.listen(3000, ()=>{
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
     console.log("serving on port 3000")
 });

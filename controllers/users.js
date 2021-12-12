b5eb20f25fe544e5cb7863c95598ac73a002e112
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-11 23:34:29 -0800

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -29,7 +29,7 @@ module.exports.login = (req,res) =>{
     req.flash('success', 'Welcome Back!');
     const redirectUrl = req.session.returnTo || '/coffeeShops'
     delete req.session.returnTo;
-    res.redirect(redirectUrl)
+    res.redirect('/')
 };
 module.exports.logout = (req,res)=>{
     req.logout();

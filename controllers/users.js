1dad2751101531a9e4b582fa4ac17d7f1bff0405
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-12 19:33:40 -0800

    express

--- controllers/users.js
+++ controllers/users.js
@@ -14,8 +14,7 @@ module.exports.register = async(req,res, next)=>{
         req.flash('success', 'Successfully Registered and Logged in')
         res.redirect('/coffeeShops')
     })
-    req.flash('success' ,'Welcome to Brewlicious!')
-    req.redirect('/coffeeShops');
+    
     } catch(e){
         req.flash('error', e.message)
         res.redirect('register');
@@ -29,7 +28,7 @@ module.exports.login = (req,res) =>{
     req.flash('success', 'Welcome Back!');
     const redirectUrl = req.session.returnTo || '/coffeeShops'
     delete req.session.returnTo;
-    res.redirect('/coffeeShops')
+    res.redirect(redirectUrl)
 };
 module.exports.logout = (req,res)=>{
     req.logout();
